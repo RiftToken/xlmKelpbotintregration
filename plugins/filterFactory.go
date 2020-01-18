449fce2a5270c978a8eefba1d4329f0791e0a659
RIFT Token <59495379+RiftToken@users.noreply.github.com>
2020-01-17 19:34:22 -0800

    Update filterFactory.go

--- plugins/filterFactory.go
+++ plugins/filterFactory.go
@@ -11,8 +11,9 @@ import (
 )
 
 var filterMap = map[string]func(f *FilterFactory, configInput string) (SubmitFilter, error){
-	"volume": filterVolume,
-	"price":  filterPrice,
+	"volume":    filterVolume,
+	"price":     filterPrice,
+	"priceFeed": filterPriceFeed,
 }
 
 // FilterFactory is a struct that handles creating all the filters
@@ -43,25 +44,28 @@ func (f *FilterFactory) MakeFilter(configInput string) (SubmitFilter, error) {
 
 func filterVolume(f *FilterFactory, configInput string) (SubmitFilter, error) {
 	parts := strings.Split(configInput, "/")
-	if len(parts) != 5 {
-		return nil, fmt.Errorf("invalid input (%s), needs 5 parts separated by the delimiter (/)", configInput)
+	if len(parts) != 6 {
+		return nil, fmt.Errorf("invalid input (%s), needs 6 parts separated by the delimiter (/)", configInput)
 	}
 
-	mode, e := parseVolumeFilterMode(parts[4])
+	mode, e := parseVolumeFilterMode(parts[5])
 	if e != nil {
 		return nil, fmt.Errorf("could not parse volume filter mode from input (%s): %s", configInput, e)
 	}
 	config := &VolumeFilterConfig{mode: mode}
-	if parts[1] != "sell" {
-		return nil, fmt.Errorf("invalid input (%s), the second part needs to be \"sell\"", configInput)
+	if parts[1] != "daily" {
+		return nil, fmt.Errorf("invalid input (%s), the second part needs to be \"daily\"", configInput)
 	}
-	limit, e := strconv.ParseFloat(parts[3], 64)
+	if parts[2] != "sell" {
+		return nil, fmt.Errorf("invalid input (%s), the third part needs to be \"sell\"", configInput)
+	}
+	limit, e := strconv.ParseFloat(parts[4], 64)
 	if e != nil {
 		return nil, fmt.Errorf("could not parse the fourth part as a float value from config value (%s): %s", configInput, e)
 	}
-	if parts[2] == "base" {
+	if parts[3] == "base" {
 		config.SellBaseAssetCapInBaseUnits = &limit
-	} else if parts[2] == "quote" {
+	} else if parts[3] == "quote" {
 		config.SellBaseAssetCapInQuoteUnits = &limit
 	} else {
 		return nil, fmt.Errorf("invalid input (%s), the third part needs to be \"base\" or \"quote\"", configInput)
@@ -99,4 +103,27 @@ func filterPrice(f *FilterFactory, configInput string) (SubmitFilter, error) {
 		return MakeFilterMaxPrice(f.BaseAsset, f.QuoteAsset, &config)
 	}
 	return nil, fmt.Errorf("invalid price filter type in second argument (%s)", configInput)
+}
+
+func filterPriceFeed(f *FilterFactory, configInput string) (SubmitFilter, error) {
+	// parts[0] = "priceFeed", parts[1] = comparisonMode, parts[2] = feedDataType, parts[3] = feedURL which can have more "/" chars
+	parts := strings.Split(configInput, "/")
+	if len(parts) < 4 {
+		return nil, fmt.Errorf("\"priceFeed\" filter needs at least 4 parts separated by the '/' delimiter (priceFeed/<comparisonMode>/<feedDataType>/<feedURL>) but we received %s", configInput)
+	}
+
+	cmString := parts[1]
+	feedType := parts[2]
+	feedURL := strings.Join(parts[3:len(parts)], "/")
+	pf, e := MakePriceFeed(feedType, feedURL)
+	if e != nil {
+		return nil, fmt.Errorf("could not make price feed for config input string '%s': %s", configInput, e)
+	}
+
+	filter, e := MakeFilterPriceFeed(f.BaseAsset, f.QuoteAsset, cmString, pf)
+	if e != nil {
+		return nil, fmt.Errorf("could not make price feed filter for config input string '%s': %s", configInput, e)
+	}
+
+	return filter, nil
 }
